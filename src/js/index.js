b4beb978b1c02e15a5b690a3cb07fb64a6b2f8d3
lauraosallor <lauraosallor@gmail.com>
2024-06-26 20:23:24 +0200

    checkform

--- src/js/index.js
+++ src/js/index.js
@@ -1,6 +1,8 @@
 // El styles lo importamos aquí, ya se carga después al compilar todo
 import '../scss/styles.scss';
 
+/*
+
 const formElmente = document.getElementById('form');
 
 const checkForm = event => {
@@ -12,3 +14,39 @@ formElmente.addEventListener('submit', checkForm);
 formElmente.addEventListener('input', event => {
   console.log(event.target.value);
 });
+
+*/
+
+const formElement = document.getElementById('form')
+const inputs = document.querySelectorAll('input')
+const nameElement = document.getElementById('card-name')
+
+
+const checkForm = event =>{
+  event.preventDefault();
+  
+  for (const input of inputs){
+    input.classList.remove('error')
+    
+    if(!input.value){
+      input.classList.add('error')
+    }else{
+      input.classList.remove('error')
+    }
+  }
+
+}
+
+formElement.addEventListener('submit', checkForm)
+
+const writeDatainCard = () =>{
+  let data = []
+  for (const input of inputs){
+    data += input.value 
+    
+  }
+
+  nameElement.textContent = data[0]
+
+}
+
