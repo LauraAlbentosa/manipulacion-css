92eebcedb436b9b81a6965baf05e7a1df76b794a
lauraosallor <lauraosallor@gmail.com>
2024-06-26 11:59:45 +0200

    html base

--- src/js/index.js
+++ src/js/index.js
@@ -1,2 +1,14 @@
 // El styles lo importamos aquí, ya se carga después al compilar todo
 import '../scss/styles.scss';
+
+const formElmente = document.getElementById('form');
+
+const checkForm = event => {
+  event.preventDefault();
+};
+
+formElmente.addEventListener('submit', checkForm);
+
+formElmente.addEventListener('input', event => {
+  console.log(event.target.value);
+});
